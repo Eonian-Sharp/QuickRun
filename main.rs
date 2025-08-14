2b3e0d3d986c372e5d4a8d4c95ebf2096068f167
伊诺 <45089051+enomothem@users.noreply.github.com>
2025-08-14 15:06:45 +0800

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,11 @@
+extern crate winres;
+
+fn main() {
+
+  if cfg!(target_os = "windows") {
+
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("icon.ico");
+    if let Err(_) = res.compile() {}
+  }
+}
